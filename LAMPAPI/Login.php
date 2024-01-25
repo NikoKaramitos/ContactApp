6b24e3873237427a9c3295a21eabc2ea12f91f97
Tri <tr530425@ucf.edu>
2024-01-25 03:01:20 -0500

    desperation part 2 electric boogaloo

--- LAMPAPI/Login.php
+++ LAMPAPI/Login.php
@@ -14,8 +14,8 @@
 	}
 	else
 	{
-		$stmt = $conn->prepare("SELECT ID,firstName,lastName FROM Users WHERE Login=? AND Password =?");
-		$stmt->bind_param("ss", $inData['Login'], $inData['Password']);
+		$stmt = $conn->prepare("SELECT ID,FirstName,LastName FROM Users WHERE Login=? AND Password =?");
+		$stmt->bind_param("ss", $inData['login'], $inData['password']);
 		$stmt->execute();
 		$result = $stmt->get_result();
 
