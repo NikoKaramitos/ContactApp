9447e7f48706701c265f040ef47184076819ce08
Santos-Matthew <matthew.e.santos47@gmail.com>
2024-01-25 21:54:20 -0500

    update SearchContacts.php mysqli

--- LAMPAPI/SearchContacts.php
+++ LAMPAPI/SearchContacts.php
@@ -2,68 +2,134 @@
 <?php
 
 	$inData = getRequestInfo();
-	
-	$searchResults = "";
-	$searchCount = 0;
 
-	$conn = new mysqli("contactz.xyz", "TheBeast", "Group31POOS", "COP4331");
-	if ($conn->connect_error) 
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+
+	if (noQueries($inData))
 	{
-		returnWithError( $conn->connect_error );
-	} 
+		returnWithError("The application JSON must contain at minimum the fields 'userID' and one query: ['firstName', 'lastName', 'phone', 'email']");
+	}
+	else if ($conn->connect_error)
+	{
+		returnWithError($conn->connect_error);
+	}
 	else
 	{
-		$stmt = $conn->prepare("select FirstName from Contacts where FirstName like ? and UserID=?");
-		$FirstName = "%" . $inData["search"] . "%";
-		$stmt->bind_param("ss", $FirstName, $inData["userId"]);
-		$stmt->execute();
-		
-		$result = $stmt->get_result();
-		
-		while($row = $result->fetch_assoc())
-		{
-			if( $searchCount > 0 )
-			{
-				$searchResults .= ",";
-			}
-			$searchCount++;
-			$searchResults .= '"' . $row["FirstName"] . '"';
+		$constructed = constructCommand($inData);
+		$stmt = $conn->prepare($constructed['c']);
+
+		// Bind dynamically generated prepared statment
+		for ($i = 0; $i < count($constructed['t']); $i++) {
+			$stmt->bind_param($constructed['t'][$i], $constructed['b'][$i]);
 		}
-		
-		if( $searchCount == 0 )
+
+		$stmt->execute();
+		$result = $statement->get_result();
+		$count = 0;
+		$search = ""; // String concatenation my inefficient beloved
+
+		if($result->num_rows > 0)
 		{
-			returnWithError( "No Records Found" );
+			foreach ($result->fetch_assoc() as $row)
+			{
+				$count++;
+				$search .= '{"id": ' . $row['ID'] . 
+					', "firstName": "' . $row['FirstName'] . 
+					'", "lastName": "' . $row['LastName'] . 
+					'", "phone": "' . $row['Phone'] . 
+					'", "email": "' . $row['Email'] . 
+				'"}';
+
+				// Don't append a comma on the last entry
+				if ($count != $result->num_rows)
+				{
+					$search .= ",";
+				}
+			}
+
+			returnWithInfo($search);
 		}
 		else
 		{
-			returnWithInfo( $searchResults );
+			returnWithError("No Contacts Found");
 		}
-		
+
 		$stmt->close();
 		$conn->close();
 	}
+
+	function constructCommand($inData): array
+	{
+		// Global
+		$base = "SELECT ID, FirstName, LastName, Phone, Email FROM Contacts WHERE UserID = ?";
+		$types = "s";
+		$binding = array();
+		array_push($binding, $inData["userID"]);
+		
+		// Dynamic
+		if (isset($inData['firstName']))
+		{
+			$types .= "s";
+			array_push($binding, '%' . $inData['firstName'] . '%');
+			$base .= ' AND FirstName LIKE ?';
+		}
+
+		if (isset($inData['lastName']))
+		{
+			$types .= "s";
+			array_push($binding, '%' . $inData['lastName'] . '%');
+			$base .= ' AND LastName LIKE ?';
+		}
+
+		if (isset($inData['phone']))
+		{
+			$types .= "s";
+			array_push($binding, '%' . $inData['phone'] . '%');
+			$base .= ' AND Phone LIKE ?';
+		}
+
+		if (isset($inData['email']))
+		{
+			$types .= "s";
+			array_push($binding, '%' . $inData['email'] . '%');
+			$base .= ' AND Email LIKE ?';
+		}
+
+		return array('c' => $base, 't' => $types, 'b' => $binding);
+	}
+
+    function noQueries($inData)
+    {
+		// NO 'userID' OR NO ONE OF ['firstName', 'lastName', 'phone', 'email']
+        return !isset($inData['userID']) || !(
+			isset($inData['firstName'])
+			|| isset($inData['lastName'])
+			|| isset($inData['phone'])
+			|| isset($inData['email'])
+		);
+    }
 
 	function getRequestInfo()
 	{
 		return json_decode(file_get_contents('php://input'), true);
 	}
 
-	function sendResultInfoAsJson( $obj )
+	function sendResultInfoAsJson($obj)
 	{
-		header('Content-type: application/json');
+		header('Content-Type: application/json');
 		echo $obj;
 	}
-	
-	function returnWithError( $err )
+
+	function returnWithError($err)
 	{
-		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		$retValue = '{"results": [], "error": "' . $err . '"}';
 		sendResultInfoAsJson( $retValue );
 	}
-	
-	function returnWithInfo( $searchResults )
+
+	function returnWithInfo($searchResults)
 	{
-		$retValue = '{"results":[' . $searchResults . '],"error":""}';
-		sendResultInfoAsJson( $retValue );
+		$retValue = '{"results": [' . $searchResults . '], "error": ""}';
+		sendResultInfoAsJson($retValue);
 	}
-	
+
 ?>
