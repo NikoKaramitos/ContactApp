7b412788d92e3ae235ed92f0a24ee5dab19d1d77
Santos-Matthew <matthew.e.santos47@gmail.com>
2024-01-25 21:52:15 -0500

    update Register.php mysqli

--- LAMPAPI/Register.php
+++ LAMPAPI/Register.php
@@ -2,25 +2,73 @@
 <?php
 
 	$inData = getRequestInfo();
-	
-	$id = 0;
-	$firstName = "";
-	$lastName = "";
-	$login = "";
-	$password = "";
 
-	$conn = new mysqli("contactz.xyz", "TheBeast", "Group31POOS", "COP4331"); 	
-	if( $conn->connect_error )
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+
+	if (invalidApplication($inData))
 	{
-		returnWithError( $conn->connect_error );
+		returnWithError("Some of the required application JSON fields: ['firstName', 'lastName', 'login', 'password'] are missing");
 	}
-	else
+	else if($conn->connect_error)
 	{
-		$stmt = $conn->prepare("INSERT INTO Users (FirstName,LastName,Login,Password) VALUES(?,?,?,?)");
-		$stmt->bind_param("ssss", $inData['firstName'],$inData['lastName'],$inData['login'],$inData['password']);
-		$stmt->execute();
+		returnWithError($conn->connect_error);
+	}
+	else if (usernameTaken($conn, $inData))
+	{
+		returnWithError("The username: ['" . $inData["login"] . "'] is already taken");
+	}
+	else // Create the user
+	{
+		$date = date("Y-m-d H:i:s");
+		$stmt = $conn->prepare("INSERT INTO Users (DateCreated, FirstName, LastName, Login, Password) VALUES (?, ?, ?, ?, ?)");
+		$stmt->bind_param("sssss", $date, $inData["firstName"], $inData["lastName"], $inData["login"], $inData["password"]);
+
+		if($stmt->execute()) // Returns true if the execution was successful
+		{
+			$id = getRegisteredId($conn, $inData);
+			returnWithInfo($id, $inData["firstName"], $inData["lastName"]);
+		}
+		else
+		{
+			returnWithError("Registration Failed: ['" . $stmt->error . "']");
+		}
+
+		// Done
 		$stmt->close();
 		$conn->close();
+	}
+
+	function getRegisteredId($conn, $inData)
+	{
+		$stmt = $conn->prepare("SELECT ID FROM Users WHERE Login = ? AND Password = ?");
+		$stmt->bind_param("ss", $inData['login'], $inData['password']);
+		$stmt->execute();
+		$result = $stmt->get_result();
+		$row = $result->fetch_assoc();
+		$stmt->close();
+
+		return $row['ID'];
+	}
+
+	function usernameTaken($conn, $inData)
+	{
+		$stmt = $conn->prepare("SELECT 1 FROM Users WHERE Login = ?");
+		$stmt->bind_param("s", $inData['login']);
+		$stmt->execute();
+		$result = $stmt->get_result();
+		$row = $result->fetch_row();
+		$stmt->close();
+
+		// If the return value is non-null then its truthy, the username already exists
+		return $row;
+	}
+
+	function invalidApplication($inData)
+	{
+		return !isset($inData['firstName'])
+			|| !isset($inData['lastName'])
+			|| !isset($inData['login'])
+			|| !isset($inData['password']);
 	}
 	
 	function getRequestInfo()
@@ -28,22 +76,23 @@
 		return json_decode(file_get_contents('php://input'), true);
 	}
 
-	function sendResultInfoAsJson( $obj )
+	function sendResultInfoAsJson($obj)
 	{
-		header('Content-type: application/json');
+		header('Content-Type: application/json');
 		echo $obj;
 	}
 	
-	function returnWithError( $err )
+	function returnWithError($err)
 	{
-		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
+		$retValue = '{"id": 0, "firstName": "", "lastName": "", "error": "' . $err . '"}';
+		sendResultInfoAsJson($retValue);
 	}
 	
-	function returnWithInfo( $firstName, $lastName, $id )
+	// Allows for automatically logging in after registering
+	function returnWithInfo($id, $firstName, $lastName)
 	{
-		$retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
-		sendResultInfoAsJson( $retValue );
+		$retValue = '{"id": ' . $id . ',"firstName": "' . $firstName . '","lastName": "' . $lastName . '", "error": ""}';
+		sendResultInfoAsJson($retValue);
 	}
 	
 ?>
