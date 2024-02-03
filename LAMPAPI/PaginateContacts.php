333520fb94687a95cbc8acc78002258952ffbe93
Santos-Matthew <matthew.e.santos47@gmail.com>
2024-02-02 21:27:36 -0500

    Trying to fix only returning one instead of 10 contacts

--- LAMPAPI/PaginateContacts.php
+++ LAMPAPI/PaginateContacts.php
@@ -33,7 +33,7 @@
 		) AS query WHERE row_num BETWEEN ? AND ?";
 
 		$stmt = $conn->prepare($command);
-		$stmt->bind_param("iii", $inData["userID"], $min, $max);
+		$stmt->bind_param("sii", $inData["userID"], $min, $max);
 		$stmt->execute();
 		$result = $stmt->get_result();
 		$count = 0;
