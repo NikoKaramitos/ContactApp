ab6bf6a4422f924ba49ddfafefe27f34ed1a2f3a
carlos-cuns <carloscunsb@gmail.com>
2024-02-07 00:45:10 -0500

    edit php

--- LAMPAPI/EditContacts.php
+++ LAMPAPI/EditContacts.php
@@ -8,11 +8,16 @@
         returnWithError ("". $conn->connect_error);
     }
 
+    /*
     $stmt = $conn->prepare("UPDATE Contacts SET FirstName = ?, LastName = ?, Phone = ?, Email = ? WHERE
     FirstName = ? AND LastName = ? AND Phone = ? AND Email = ? AND UserID = ?");
-
     $stmt->bind_param("ssssssssi", $inData["oldFirstName"], $inData["oldLastName"], $inData["oldPhone"], $inData["oldEmail"]
     , $inData["newFirstName"], $inData["newLastName"], $inData["newPhone"], $inData["newEmail"], $inData["userId"]);
+    */
+    $stmt = $conn->prepare("UPDATE Contacts SET ID = ?, UserID = ? WHERE FirstName = ?, LastName = ?, Phone = ?, Email = ?");
+
+    $stmt->bind_param("s", $inData["contactID"], $inData["userID"], $inData["newFirstName"], $inData["newLastName"], $inData["newPhone"], $inData["newEmail"]);
+    
 
     if ($stmt->execute()) {
         if ($stmt->affected_rows > 0) {
