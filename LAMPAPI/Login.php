46db1c69d51468110284c12de2ca723670d8d932
Tri <tr530425@ucf.edu>
2024-02-07 23:35:36 -0500

    CORS

--- LAMPAPI/Login.php
+++ LAMPAPI/Login.php
@@ -1,6 +1,6 @@
 
 <?php
-	header('Access-Control-Allow-Origin: http://127.0.0.1:54417/login.html');
+	header('Access-Control-Allow-Origin: http://127.0.0.1:54471/login.html');
 	header('Access-Control-Allow-Credentials: true');
 	header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
 	header('Access-Control-Allow-Headers: X-Requested-With, Content-Type, Accept, Origin, Authorization');
